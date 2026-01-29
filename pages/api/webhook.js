71b9922b654798a5bbc9085835ab841cff50e515
lukamacura <designmacura@gmail.com>
2026-01-29 22:05:19 +0100

    novi link za disc

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -58,7 +58,7 @@ export default async function handler(req, res) {
 
     // Discord linkovi po paketima
     const linkMap = {
-      premium: "https://discord.gg/beW76jF3KN",
+      premium: "https://discord.gg/PYFjjuSEYB",
       coaching: "https://discord.gg/z3y2TQ9EfZ",
     };
     const discordLink = linkMap[pkg];
